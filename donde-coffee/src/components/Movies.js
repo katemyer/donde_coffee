37e2ec2999ead0d7eedf12ea899755f4cecb4937
Antonia <antonianomamiukor@gmail.com>
2020-07-08 23:24:05 -0700

    setup for flask/react integration

---
+++ donde-coffee/src/components/Movies.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { List, Header } from "semantic-ui-react";
+
+export const Movies = ({ movies }) => {
+  return (
+    <List>
+      {movies.map(movie => {
+        return (
+          <List.Item key={movie.title}>
+            <Header>{movie.title}</Header>
+
+
+          </List.Item>
+        )
+      })}
+    </List>
+  )
+}
