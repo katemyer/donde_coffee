066f300b10ecd9e69390e572a127b005be2622bd
kate myer <kate.d.mangubat@gmail.com>
2020-07-08 22:47:05 -0700

    hooked up skeleton sqlite database

---
+++ README.md
@@ -0,0 +1,11 @@
+# donde_coffee
+Mission: Donde all the coffee?!
+
+We just want to find good coffee while we travel from Portland, OR to Vancouver, B.C. Is that too much to ask for?
+
+# Tech Stack
+### Backend: Python Flask API
+### Frontend: React
+### Local DB: SQLITE
+### Extermal DB: Trip Advisor
+### Coffee of choice during this capstone: All of the coffees
--- api/views.py
+++ api/views.py
@@ -1,26 +1,39 @@
 from flask import Blueprint, jsonify, request
-# from . import db 
-# from .models import Movie
+from . import db # . looks in the __init__ file
+from .models import Movie #.models looks in models file
 
 main = Blueprint('main', __name__)
 
+#POST /add_movie
+#Goal: taking data from request, adding to db, commiting
+
 @main.route('/add_movie', methods=['POST'])
 def add_movie():
-    movie_data = request.get_json()
+  movie_data = request.get_json()
 
-    # new_movie = Movie(title=movie_data['title'], rating=movie_data['rating'])
+  #new Movie object
+  #title comes from movie_data that's a json
+  #the key for title is 'title'
+  new_movie = Movie(title=movie_data['title'], rating=movie_data['rating'])
 
-    # db.session.add(new_movie)
-    # db.session.commit()
+  db.session.add(new_movie) #adding new movie
+  db.session.commit() # .save in rails
 
-    return 'Done', 201
+  return 'Done', 201
 
+#GET /movies
+#list all movies
 @main.route('/movies')
 def movies():
-    movies = ['hi']
-    # movie_list = Movie.query.all()
-    # for movie in movie_list:
-    #     movies.append({'title' : movie.title, 'rating' : movie.rating})
+    #sqlite query directly on class Movie, get all movies
+    movie_list = Movie.query.all()
+    movies = []
+    
+    #append dictionary to movies [] with data: title and rating
+    #movie_list here matches movie_list variable that was set for the db query 
+    for movie in movie_list:
+        movies.append({'title' : movie.title, 'rating' : movie.rating})
+
     return jsonify({'movies' : movies})
 
 #index page
