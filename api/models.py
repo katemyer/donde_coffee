998a10e1409c9040acd170f45defe55a10c75867
kate myer <kate.d.mangubat@gmail.com>
2020-07-09 23:46:31 -0700

    created test user to seed to db functionality

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from . import db #import db object to inherent from class in __init__ file
 
-class Movie(db.Model):
+class User(db.Model):
   #table columns
   id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(50))
-  rating = db.Column(db.Integer)
+  name = db.Column(db.String(50))
+  email = db.Column(db.String(50))
