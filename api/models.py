dae9e3ba5e7245c6318ba22a56908adc4442f809
Antonia <antonianomamiukor@gmail.com>
2020-07-10 13:57:16 -0700

    setup for shop db table and user-shop relationship

--- api/models.py
+++ api/models.py
@@ -1,7 +1,23 @@
 from . import db #import db object to inherent from class in __init__ file
+from datetime import datetime
 
 class User(db.Model):
   #table columns
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(50))
   email = db.Column(db.String(50))
+  saved_shops = db.relationship('Post', backref='person', lazy=True)
+
+  def __repr__(self):
+    return f"User('{self.name}', '{self.email}')"
+
+class Shop(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(120))
+  address = db.Column(db.String(120))
+  hours = db.Column(db.String(120))
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+  def __repr__(self):
+    return f"Shop('{self.name}', '{self.address}', '{self.address}'), '{self.hours}'"
+
