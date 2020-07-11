32564d86d6af34283427ee4aa26b3f2e4e1d1e56
kate myer <kate.d.mangubat@gmail.com>
2020-07-10 22:26:31 -0700

    corrected users db table, added shop test data

--- api/models.py
+++ api/models.py
@@ -4,21 +4,22 @@ from datetime import datetime
 
 class User(db.Model):
     # table columns
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(50))
-    saved_shops = db.relationship('Post', backref='person', lazy=True)
+    # saved_shops = db.relationship('Post', backref='person', lazy=True)
 
-    def __repr__(self):
-        return f"User('{self.name}', '{self.email}')"
+    def __repr__(self):    
+      return f"User('{self.name}', '{self.email}')"
 
 
-class Shop(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    hours = db.Column(db.String(120))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+# class Shop(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(120))
+#     address = db.Column(db.String(120))
+#     hours = db.Column(db.String(120))
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __repr__(self):
-        return f"Shop('{self.name}', '{self.address}', '{self.address}'), '{self.hours}'"
+#     def __repr__(self):
+#         return f"Shop('{self.name}', '{self.address}', '{self.address}'), '{self.hours}'"
