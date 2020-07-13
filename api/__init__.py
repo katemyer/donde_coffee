189fa26725098a346fd0c4a8b4a566c108d4c47b
kate myer <kate.d.mangubat@gmail.com>
2020-07-13 14:50:59 -0700

    one to many relationship and tables

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,35 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 
+# from config import Config
+from flask_migrate import Migrate
+
+# app = Flask(__name__)
+# app.config.from_object(Config)
+# db = SQLAlchemy(app)
 db = SQLAlchemy() #don't pass anything bc initialize later
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
     db.init_app(app) #initialize app here
+    migrate = Migrate(app, db)
+
     from .views import main
     app.register_blueprint(main)
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
+    
     return app
--- api/models.py
+++ api/models.py
@@ -1,18 +1,17 @@
 from . import db  # import db object to inherent from class in __init__ file
 from datetime import datetime
 
-
+# UserMixin, 
 class User(db.Model):
-  # table columns
-  __tablename__ = 'users'
+  # __tablename__ = 'users'
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(50))
-  email = db.Column(db.String(50))
-  # saved_shops = db.relationship('Post', backref='person', lazy=True)
+  username = db.Column(db.String(64), index=True, unique=True)
+  email = db.Column(db.String(120), index=True, unique=True)
+  password_hash = db.Column(db.String(128))
+  posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-  # def __repr__(self):    
-  #   return f"User('{self.name}', '{self.email}')"
-
+  def __repr__(self):
+      return '<User {}>'.format(self.username)
 
 class Shop(db.Model):
   __tablename__ = 'shops'
@@ -28,3 +27,12 @@ class Shop(db.Model):
 
   # def __repr__(self):
   #     return f"Shop('{self.name}', '{self.address}', '{self.address}'), '{self.hours}'"
+  
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
