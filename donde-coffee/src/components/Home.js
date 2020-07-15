6c1065ddb1c21088e2456265cd78d47d77ba2e4e
Antonia <antonianomamiukor@gmail.com>
2020-07-14 22:20:57 -0700

    fixed bug preventing redirect after registration

--- donde-coffee/src/components/Home.js
+++ donde-coffee/src/components/Home.js
@@ -2,11 +2,24 @@ import React, { Component } from "react";
 import Registration from "./auth/Registration";
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+  }
+
+  handleSuccessfulAuth(data) {
+    this.props.handleLogin(data);
+    this.props.history.push("/dashboard")
+    console.log("hi")
+  }
+
   render() {
     return (
       <div>
         <h1>Home</h1>
-        <Registration />
+        <h2>status: {this.props.loggedInStatus}</h2>
+        <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
       </div>
     )
   }
